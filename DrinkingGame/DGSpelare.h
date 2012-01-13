bc9ebdb3a1688d66c7bb21cda8a788dba9214a3d
Andrej Scherbakov-Parland <parland@dhcp34-142.arcada.fi>
2012-01-13 14:53:56 +0200

    Start for DGSpelare class

---
+++ DrinkingGame/DGSpelare.h
@@ -0,0 +1,13 @@
+//
+//  DGSpelare.h
+//  DrinkingGame
+//
+//  Created by Andrej Scherbakov-Parland on 1/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DGSpelare : NSObject
+
+@end
---
+++ DrinkingGame/DGSpelare.m
@@ -0,0 +1,13 @@
+//
+//  DGSpelare.m
+//  DrinkingGame
+//
+//  Created by Andrej Scherbakov-Parland on 1/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DGSpelare.h"
+
+@implementation DGSpelare
+
+@end
