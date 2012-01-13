6005981c8d60885bc60bac1d43a84eeae6b482b8
student <student@dhcp32-106.arcada.fi>
2012-01-13 14:48:13 +0200

    JEsn commitade alko filer

---
+++ DrinkingGame/Alkocounter.m
@@ -0,0 +1,23 @@
+//
+//  Alkocounter.m
+//  DrinkingGame
+//
+//  Created by student on 1/13/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Alkocounter.h"
+//and here
+@implementation Alkocounter
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
