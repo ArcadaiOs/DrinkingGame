f0a6ac687f72b651dc50408c81599e8b8d4c8a18
straend <tomas@ae-servu.net>
2012-01-13 13:39:18 +0200

    first commit

---
+++ DrinkingGame/DrinkingGameAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DrinkingGameAppDelegate.h
+//  DrinkingGame
+//
+//  Created by student on 1/13/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DrinkingGameViewController;
+
+@interface DrinkingGameAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet DrinkingGameViewController *viewController;
+
+@end
---
+++ DrinkingGame/DrinkingGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  DrinkingGameViewController.h
+//  DrinkingGame
+//
+//  Created by student on 1/13/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrinkingGameViewController : UIViewController
+
+@end
