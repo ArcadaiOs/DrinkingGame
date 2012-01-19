17321b95c6ce16002ec4579b8c26df941162e434
straend <tomas@ae-servu.net>
2012-01-19 13:29:44 +0200

    DGSpelare -> DGPlayer DGGAme

---
+++ DrinkingGame/DGGame.m
@@ -0,0 +1,23 @@
+//
+//  DGGame.m
+//  DrinkingGame
+//
+//  Created by student on 1/19/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DGGame.h"
+
+@implementation DGGame
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
---
+++ DrinkingGame/DGPlayer.m
@@ -0,0 +1,18 @@
+//
+//  DGSpelare.m
+//  DrinkingGame
+//
+//  Created by Andrej Scherbakov-Parland on 1/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DGPlayer.h"
+
+@implementation DGPlayer
+
+@synthesize gender;
+@synthesize weight;
+@synthesize picture;
+@synthesize alcoholQuantity;
+
+@end
