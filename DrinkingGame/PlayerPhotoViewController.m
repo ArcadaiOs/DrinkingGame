1163d0bdf9532a01d73b42b8046a27326ce4c54b
student <student@dhcp35-251.arcada.fi>
2012-01-20 14:57:49 +0200

    a

---
+++ DrinkingGame/PlayerPhotoViewController.m
@@ -0,0 +1,13 @@
+//
+//  PlayerPhotoViewController.m
+//  DrinkingGame
+//
+//  Created by student on 1/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PlayerPhotoViewController.h"
+
+@implementation PlayerPhotoViewController
+
+@end
