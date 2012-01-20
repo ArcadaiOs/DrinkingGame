5f34d7695d9c36b6eef84067062701d8ada4c760
student <student@dhcp35-251.arcada.fi>
2012-01-20 13:52:08 +0200

    db

---
+++ DrinkingGame/DGPlayerModel.h
@@ -0,0 +1,13 @@
+//
+//  DGPlayerModel.h
+//  DrinkingGame
+//
+//  Created by student on 1/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DGPlayerModel : NSObject
+
+@end
---
+++ DrinkingGame/DGPlayerModel.m
@@ -0,0 +1,13 @@
+//
+//  DGPlayerModel.m
+//  DrinkingGame
+//
+//  Created by student on 1/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DGPlayerModel.h"
+
+@implementation DGPlayerModel
+
+@end
