19ee28f5d100c0d6a87e28ed4507ff7732a3e59f
student <student@dhcp35-155.arcada.fi>
2012-02-02 13:37:47 +0200

    Committa twå nya spel -Jon

---
+++ DrinkingGame/DGSteadyHands.m
@@ -0,0 +1,65 @@
+//
+//  DGSteadyHands.m
+//  DrinkingGame
+//
+//  Created by student on 2/2/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DGSteadyHands.h"
+
+@implementation DGSteadyHands
+@synthesize labelX,labelY,labelZ,progressX,progressY,progressZ;
+@synthesize accelerometer;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    self.accelerometer = [UIAccelerometer sharedAccelerometer];
+    self.accelerometer.updateInterval = .1;
+    self.accelerometer.delegate = self;
+    return self;
+}
+- (void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration {
+    labelX.text = [NSString stringWithFormat:@"%@%f", @"X: ", acceleration.x];
+    labelY.text = [NSString stringWithFormat:@"%@%f", @"Y: ", acceleration.y];
+    labelZ.text = [NSString stringWithFormat:@"%@%f", @"Z: ", acceleration.z];
+    //accelvalue+=acceleration.x;
+    //accelvalue+=acceleration.y;
+    //accelvalue+=acceleration.z;
+    self.progressX.progress = ABS(acceleration.x);
+    self.progressY.progress = ABS(acceleration.y);
+    self.progressZ.progress = ABS(acceleration.z);
+}
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
---
+++ DrinkingGame/DGTestGameTwo.h
@@ -0,0 +1,13 @@
+//
+//  DGTestGameTwo.h
+//  DrinkingGame
+//
+//  Created by student on 2/2/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DGTestGameTwo : UIViewController
+
+@end
