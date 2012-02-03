9d2d94a35a78310a639c949469e3af958d908b91
student <student@dhcp35-155.arcada.fi>
2012-02-03 14:13:27 +0200

    l

--- DrinkingGame/DGTestGameTwo.h
+++ DrinkingGame/DGTestGameTwo.h
@@ -11,7 +11,7 @@
 
 @interface DGTestGameTwo : DGGame{
 
+    NSMutableArray *difficulty;
     
 }
-
 @end
--- DrinkingGame/DGTestGameTwo.m
+++ DrinkingGame/DGTestGameTwo.m
@@ -23,7 +23,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+
 }
 
 - (void)viewDidUnload
