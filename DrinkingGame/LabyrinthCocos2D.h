8044338cc37370e03a61eb9dd493099ddd5e25b0
student <student@dhcp35-154.arcada.fi>
2012-02-10 14:16:41 +0200

    test

---
+++ DrinkingGame/LabyrinthCocos2D.h
@@ -0,0 +1,17 @@
+//
+//  LabyrinthCocos2D.h
+//  DrinkingGame
+//
+//  Created by student on 2/10/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface LabyrinthCocos2D : CCLayer {
+
+}
++(CCScene *) scene;
+
+@end
