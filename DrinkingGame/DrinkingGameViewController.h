2598d5692ce2bc99580980de7548d315b5062374
straend <tomas@ae-servu.net>
2012-02-24 12:54:51 +0200

    lost

--- DrinkingGame/DrinkingGameViewController.h
+++ DrinkingGame/DrinkingGameViewController.h
@@ -29,43 +29,54 @@ typedef struct{
 
 
 @interface DrinkingGameViewController : UIViewController{
-    DGGame *currentCame;
+    DGGame *currentGame;
     DGController* controller;
     
     //twitter
-       
-//    IBOutlet UIButton *twitterButton;
+    //IBOutlet UIButton *twitterButton;
+    
+    
+    IBOutlet UIView *playerLostView;
     IBOutlet UIView *boozeChooserView;
     IBOutlet UIView*boo;
+    
+    IBOutlet UILabel *playerNameLabel;    
     IBOutlet UIView *playerImgFrame;    
     IBOutlet UIImageView *playerImg;
     IBOutlet UIView *nextPlayerView;
+
     id <ViewControllerProto> delegate;
     UIViewController *viewControl;
+    
+    IBOutlet UIView *debugView;
 }
 
 @property (readonly, nonatomic) DGController* controller;
 @property(nonatomic,assign)id delegate;
-
-    //twitter
-/*@property (retain) UIButton *twitterButton;
--(IBAction)twitterAction:(id)sender;*/
-
 -(DGController *) controller;
+
+//twitter
+/*
+@property (retain) UIButton *twitterButton;
+-(IBAction)twitterAction:(id)sender;
+*/
+
 
 -(IBAction) showPunnishmentChooser:(id)sender;
 -(IBAction) punnishmentChosen:(id) sender;
-
+-(void) gameEndedWithLooser:(DGPlayer*) player;
 -(void) showPlayer:(DGPlayer*)player;
--(void) playerReadyToPlay;
--(IBAction) showNext:(id)sender;
--(void) gameEndedWithScores:(NSString*) scores;
--(IBAction) launchTestGame:(id)sender;
+
+-(IBAction) launchLOOSER:(id)sender;
+
+-(void) launchGame:(DGGame*)game;
+
+-(IBAction) launchRandomShot:(id)sender;
 -(IBAction) launchSimon:(id)sender;
 -(IBAction) launchSteadyHands:(id)sender;
 -(IBAction) launchWhackAMole:(id)sender;
 -(IBAction) launchFillTheBottle:(id)sender;
 -(IBAction) launchMenu:(id)sender;
 -(IBAction) stopMusic:(id)sender;
--(void)handleTap:(UITapGestureRecognizer *)sender;
+//-(void)handleTap:(UITapGestureRecognizer *)sender;
 @end
